27ba86d0d50e7b818af9d95999a78f052471d2dd
Maynal Haque <43097152+moynalhaque@users.noreply.github.com>
2019-04-15 12:45:39 +0600

    Array_Test

---
+++ ArrayPractice/src/ArrayTest.java
@@ -0,0 +1,17 @@
+
+public class ArrayTest {
+public static void main(String[] args) {
+
+		String array[]= {"Maynal Haque", "23 ", "318 ", "Trainee ", "0184 "};
+		//System.out.println(array[1]);
+		
+		for(int i= 0; i<=4; i++) {
+			System.out.println(array[i]);
+		}
+		
+//		for(int i= 0; i<=array[i].length; i++) {
+//			System.out.println(array[i]);
+//		}
+
+	}
+}
